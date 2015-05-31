a21429ff0bfa495f6b45d636b53af069bce7627a
ThorbenR <ThorbenR@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-31 10:46:38 +0000

    Add Version 1.0.4
    git-svn-id: https://plugins.svn.wordpress.org/wp-piwik-privacy-by-wpado/trunk@1171276 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -3,18 +3,39 @@ Contributors: thorbenr
 Tags: piwik, analytics, privatsphäre, privacy
 Requires at least: 3.2
 Tested up to: 4.2.2
-Stable tag: 1.0.3
+Stable tag: 1.0.4
 License: GPLv2
 
-Erstellt den Shortcode [wpado_piwik_privacy] um das Tracking von dem Analyse-Tool Piwik zu deaktivieren.
+Mit diesem Plugin kann der Besucher den Trackingprozress des Analyse-Tools Piwik deaktivieren.
 
 == Description ==
 
-Es wird vorausgesetzt, dass das Plugin [WP-Piwik](http://wordpress.org/extend/plugins/wp-piwik/) installiert und aktiviert ist. Es wird dem Besucher ermöglicht, das Tracking von dem Analyse-Tool Piwik zu deaktivieren. 
-Der Seiteninhaber kann die Deaktivierungsmöglichkeit mit dem Shortcode [wpado_piwik_privacy] einfügen.
+Mit diesem Plugin kann der Besucher den Trackingprozress des Analyse-Tools [Piwik](http://piwik.org/) deaktivieren.
 
-Weitere Informationen erhalten Sie unter:
- http://www.wp-agentur-do.de/referenzen-eintrag/wp-piwik-privacy-by-wpado/
+Der Seiteninhaber kann die Deaktivierungsmöglichkeit mit dem Shortcode 
+    [wpado_piwik_privacy]
+einfügen.
+
+**Des Weiteren können folgende Optionen ausgewählt werden:**
+* css: "yes" oder "no" (Wenn das Piwik-Plugin "CustomOptOut" installiert und aktiviert ist, kann die Ausgabe angepasst werden)
+* width: z.B. "100%"
+* height: z.B. "200px"
+
+Der Shortcode sieht dann wie folgt aus:
+    [wpado_piwik_privacy css="yes" width="100%" height="200px"]
+
+**Voraussetzungen:**
+* [PHP](http://php.net/) 5.4 oder aktueller
+* [WordPress](https://wordpress.org/download/) 3.2 oder aktueller
+* [Piwik](http://piwik.org/) 2.9 oder aktueller
+* [WP-Piwik](http://wordpress.org/extend/plugins/wp-piwik/) 1.0.1
+
+Um die Ausgabe grafisch anzupassen, wird das [Piwik-Plugin "CustomOptOut"](http://plugins.piwik.org/customoptout) vorausgesetzt.
+
+Weitere Informationen erhalten Sie auf der Projektseite:
+http://www.wp-agentur-do.de/referenzen-eintrag/wp-piwik-privacy-by-wpado/
+
+Dieses Plugin wurde nicht vom offiziellen Piwik-Team erstellt und wird somit auch nicht von deren Seite zur Verfügung gestellt.
 
 == Installation ==
 
@@ -38,6 +59,12 @@ Bei Fragen nutze Sie bitte das Kontaktformular unter [http://www.wp-agentur-do.d
 Keine.
 
 == Changelog ==
+
+= 1.0.4 =
+* Datum: 31.05.2015
+* Unterstützung WP-Piwik 1.0.1 (https://wordpress.org/plugins/wp-piwik/)
+* Bugs behoben
+* 
 
 = 1.0.3 =
 * Datum: 30.05.2015
--- wpado-piwik-privacy.php
+++ wpado-piwik-privacy.php
@@ -3,71 +3,64 @@
  * Plugin Name: WP-Piwik Privacy by WPADO
  * Plugin URI: http://www.wp-agentur-do.de/referenzen-eintrag/wp-piwik-privacy-by-wpado/
  * Description: Erstellt den Shortcode <code>[wpado_piwik_privacy]</code> um das Tracking von dem Analyse-Tool Piwik (http://piwik.org/) zu deaktivieren.
- * Version: 1.0.3
+ * Version: 1.0.4
  * Author: WP Agentur Dortmund
  * Author URI: http://www.wp-agentur-do.de
  * License: GPL2
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
 
-if( ! class_exists( 'wpado_piwik_privacy' ) ) {
+function wpado_piwik_privacy_func( $atts ) {
 
-	class wpado_piwik_privacy {
+	$atts = shortcode_atts( array(
+													'css'			=>	'no',
+													'height'	=>	'200px',
+													'width'		=>	'100%'
+													), 
+													$atts, 
+													'wpado_piwik_privacy'
+												);
 
-		function wpado_piwik_privacy() {
+	if( ! class_exists( 'wp_piwik' ) ) {
 
-			self::__construct();
+		return;
+
+	}
+
+	$PiwikUrl			=	get_option('wp-piwik_global-piwik_url');
+	$PiwikSiteID	=	get_option('wp-piwik-site_id');
+
+	if( empty( $PiwikUrl ) ) {
+
+		return;
+
+	} else {
+
+		if ( function_exists('icl_object_id') ) {
+
+			$lang = ICL_LANGUAGE_CODE;
+
+		} else {
+
+			$lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
 
 		}
 
-		function __construct() {
+		if ( $atts['css'] == "yes" ) {
 
-			add_shortcode('wpado_piwik_privacy', array(
-				$this,
-				'wpado_piwik_privacy_shortcode'
-			));
+			$PiwikUrl = esc_url($PiwikUrl . '?module=CustomOptOut&action=optOut&idSite=' . $PiwikSiteID . '&language=' . $lang);
 
-		}
+		} else {
 
-		function wpado_piwik_privacy_shortcode() {
-
-			if( ! class_exists( 'wp_piwik' ) ) {
-
-				return;
-
-			}
-
-			$PiwikUrl			=	get_option('wp-piwik_global-piwik_url');
-			$PiwikSiteID	=	get_option('wp-piwik-site_id');
-
-			if( empty( $PiwikUrl ) ) {
-
-				return;
-
-			} else {
-
-				if ( function_exists('icl_object_id') ) {
-
-					$lang = ICL_LANGUAGE_CODE;
-
-				} else {
-
-					$lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
-
-				}
-
-				$PiwikUrl = esc_url($PiwikUrl . '?module=CustomOptOut&action=optOut&idSite=' . $PiwikSiteID . '&language=' . $lang);
-
-			}
-
-			$out .= '<iframe id="wpado_piwik_privacy" src="' . $PiwikUrl . '" height="220" width="100%" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>';
-
-			return $out;
+			$PiwikUrl = esc_url($PiwikUrl . '?module=CoreAdminHome&action=optOut&language=' . $lang);
 
 		}
 
 	}
 
-}
+	$out .= '<iframe id="wpado_piwik_privacy" src="' . $PiwikUrl . '" height="' . $atts['height'] . '" width="' . $atts['width'] . '" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>';
 
-new wpado_piwik_privacy();
+	return $out;
+
+}
+add_shortcode( 'wpado_piwik_privacy', 'wpado_piwik_privacy_func' );
